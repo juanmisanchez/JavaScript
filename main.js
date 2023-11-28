f38680e8d3d9ce49a185e8e6a03806bfb4a4d626
juansanchez <juansanchezav@outlook.com>
2023-11-28 20:28:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+let num1 = Number(prompt("Ingresar numero"));
+let num2 = Number(prompt("Ingresar numero"));
+
+console.log( num1 + num2);
---
+++ nombreIngresado.js
@@ -0,0 +1,4 @@
+console.log("Mensaje de prueba");
+
+let nombre = "John Doe"
+console.log(nombre)
