47134896d86234ae10b414137025b73a55c21c7f
juansanchez <juansanchezav@outlook.com>
2023-12-05 17:01:51 -0300

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+let pinturaBlanca = 100;
+let pinturaNegra = 100;
+const pinturaXambiente = 20;
 
-let num1 = Number(prompt("Ingresar numero"));
-let num2 = Number(prompt("Ingresar numero"));
+while((pinturaBlanca > 0) && (pinturaNegra > 0)) {
+    let pinturablancaXambiente = Number(prompt("Ambientes con pintura blanca"));
+    let pinturanegraXambiente = Number(prompt("Ambientes con pintura negra"));
 
-console.log( num1 + num2);
+    if (pinturablancaXambiente < pinturanegraXambiente ){
+        pinturaBlanca = pinturaBlanca - (pinturablancaXambiente * pinturaXambiente)
+        console.log("Te queda " + pinturaBlanca + " de pintura blanca");
+    } 
+    else { 
+        pinturaNegra = pinturaNegra - (pinturanegraXambiente * pinturaXambiente)
+        console.log("Te queda " + pinturaNegra + " de pintura negra") 
+    }
+
+    function ambientesPintados(ambientesBlanco, ambientesNegro){
+    resultado = ambientesBlanco + ambientesNegro
+    }
+    function mostrar(mensaje){
+        console.log(mensaje)
+    }
+
+    ambientesPintados(pinturablancaXambiente, pinturanegraXambiente )
+    mostrar(resultado)
+}
